ed674e024b02f288f0610b932f3a843b1159686a
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2025-06-22 14:29:41 +0100

    feat: added an transform interceptor and api documentation to tenants module

--- src/tenants/dto/create-tenant.dto.ts
+++ src/tenants/dto/create-tenant.dto.ts
@@ -1,7 +1,14 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTenantDto {
+  @ApiProperty({
+    example: 'Obiora & Sons',
+    description: 'Name of the tenant',
+    required: true,
+  })
   @IsString({ message: 'Name must be a string' })
   @IsNotEmpty({ message: 'Name is required' })
+  @MinLength(3, { message: 'Name must be at least 3 characters' })
   name: string;
 }
